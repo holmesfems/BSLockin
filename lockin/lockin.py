ea7ccc0b18e7ce475b143a4d1fefaf54ea918129
sai <zhaiguangyuanxp@hotmail.com>
2018-05-26 02:27:00 +0900

    seperate params to another file

--- lockin/lockin.py
+++ lockin/lockin.py
@@ -10,20 +10,7 @@ import datetime
 import math
 import progbar
 import time
-#Params
-lockin_dt = 0.25
-mkidDefaultShift = 0.0
-BSFreq = 10.0
-DAQFreq = 80000.0
-FSPFreq = 64.0
-BSSE_TimeSpanRate = 10
-LSE_TimeSpanRate = 0.55
-mkidShiftErr = 0.5
-mkidShiftStep = 0.001
-ConvolStep = 0.02
-MaskRate = 0.025
-mkidForceCount = 10
-rejectRate = 2
+from param import *
 #Shared Memories
 bs_start = 0.0
 bs_end = 0.0
---
+++ lockin/param.py
@@ -0,0 +1,14 @@
+#Params
+lockin_dt = 0.25
+mkidDefaultShift = 0.0
+BSFreq = 10.0
+DAQFreq = 80000.0
+FSPFreq = 64.0
+BSSE_TimeSpanRate = 10
+LSE_TimeSpanRate = 0.55
+mkidShiftErr = 0.1
+mkidShiftStep = 0.001
+ConvolStep = 0.02
+MaskRate = 0.025
+mkidForceCount = 10
+rejectRate = 2
