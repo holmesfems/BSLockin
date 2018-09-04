93cdc910571af4637c405e2e7d52ef7234b02d1c
sai <holmesfems@gmail.com>
2018-09-05 03:33:51 +0900

    Make it support manual parameter specificatioin

--- lockin/lockin.py
+++ lockin/lockin.py
@@ -11,12 +11,13 @@ import datetime
 import math
 import progbar
 import time
-from param import *
+import param
 #Shared Memories
 bs_start = 0.0
 bs_end = 0.0
 lockin_start = 0.0
 lockin_end = 0.0
+Param = param.Param.copy()
 
 def linear_fmin(func,start,end,step):
     min = func(start)
@@ -54,18 +55,18 @@ def findSE(arr2d,freq,timeSpanRate):
             break
     return (start,end)
 
-def isInMask(point, bsArr, bsFreq, MaskRate):
-    if (numpy.abs(bsArr - point) < (MaskRate / bsFreq)).any():
+def isInMask(point, bsArr, bsfreq, maskrate):
+    if (numpy.abs(bsArr - point) < (maskrate / bsfreq)).any():
         return True
     return False
 
-def lockinBS(bsFile, oppsFile, force=False):
+def lockinBS(bsFile, oppsFile):
     global bs_start
     global bs_end
     global lockin_start
     global lockin_end
     bsArr_new = []
-    if not force and os.path.exists(bsFile+".calib"):
+    if not Param['force'] and os.path.exists(bsFile+".calib"):
         print("Calib file detected!")
         bsArr_new = numpy.loadtxt(bsFile + ".calib")
     else:
@@ -75,19 +76,19 @@ def lockinBS(bsFile, oppsFile, force=False):
         #Calculate calibrated DAQ Sampling Frequency
         firstPulse = -1
         for i in range(0,len(oppsArr)):
-            if (oppsArr[i][1] == 1) and (oppsArr[i+1][1] == 1) and (( -oppsArr[i][0] + oppsArr[i+1][0]) / DAQFreq < 0.1):
+            if (oppsArr[i][1] == 1) and (oppsArr[i+1][1] == 1) and (( -oppsArr[i][0] + oppsArr[i+1][0]) / Param['DAQFreq'] < 0.1):
                 firstPulse = oppsArr[i][0]
                 break
         lastPulse = -1
         for i in range(0,len(oppsArr)):
             i = len(oppsArr) - i - 2
-            if (oppsArr[i][1] == 1) and (oppsArr[i+1][1] == 1) and (( -oppsArr[i][0] + oppsArr[i+1][0]) / DAQFreq < 0.1):
+            if (oppsArr[i][1] == 1) and (oppsArr[i+1][1] == 1) and (( -oppsArr[i][0] + oppsArr[i+1][0]) / Param['DAQFreq'] < 0.1):
                 lastPulse = oppsArr[i][0]
                 break
         if firstPulse < 0 or lastPulse < 0:
             print("Failed to fit opps pulse!")
             return
-        span = round((lastPulse-firstPulse)/DAQFreq)
+        span = round((lastPulse-firstPulse)/Param['DAQFreq'])
         DAQFreq_calib = (lastPulse - firstPulse) / span
     
         #Calculate Zero point of time:
@@ -119,7 +120,7 @@ def lockinBS(bsFile, oppsFile, force=False):
         print("Output calibrated beamswitch signal done!")
 
     #Get time that BS starts
-    bs_start,bs_end = findSE(bsArr_new, BSFreq, BSSE_TimeSpanRate)
+    bs_start,bs_end = findSE(bsArr_new, Param['BSFreq'], Param['BSSE_TimeSpanRate'])
     if bs_end > bs_start:
         print("Found BS start point and end point")
         print("BS_Start = ", bs_start)
@@ -127,20 +128,20 @@ def lockinBS(bsFile, oppsFile, force=False):
     else:
         print("Can't find BS start point and end point")
         return
-    lockin_start,lockin_end = findSE(bsArr_new, BSFreq, LSE_TimeSpanRate)
-    lockin_start = math.ceil(lockin_start / lockin_dt) * lockin_dt
-    lockin_end = math.floor(lockin_end / lockin_dt) * lockin_dt
+    lockin_start,lockin_end = findSE(bsArr_new, Param['BSFreq'], Param['LSE_TimeSpanRate'])
+    lockin_start = math.ceil(lockin_start / Param['lockin_dt']) * Param['lockin_dt']
+    lockin_end = math.floor(lockin_end / Param['lockin_dt']) * Param['lockin_dt']
     
     return (bsArr_new,scipy.interpolate.interp1d(bsArr_new[:,0],bsArr_new[:,1]))
 
-def lockin(bsArr,interp_BS, mkidFile, force = False, shiftTime = None):
+def lockin(bsArr,interp_BS, mkidFile, shiftTime = None):
     mkidArr = []
     mkidShift = 0.0
     print("Lockin File:",mkidFile,flush = True)
     doCalib = True
     corr_isPositive = None
     #generate shift time
-    if not force and os.path.exists(mkidFile + ".calib"):
+    if not Param['force'] and os.path.exists(mkidFile + ".calib"):
         print("Calib file detected!",flush = True)
         mkidArr = numpy.loadtxt(mkidFile + ".calib")
         mkidShift = mkidArr[0][0]
@@ -157,9 +158,9 @@ def lockin(bsArr,interp_BS, mkidFile, force = False, shiftTime = None):
 
     
     if doCalib:
-        mkidArr = numpy.loadtxt(mkidFile)[:,0:3:2] / [FSPFreq,1]
+        mkidArr = numpy.loadtxt(mkidFile)[:,0:3:2] / [Param['FSPFreq'],1]
         #Get calibrated mkid shift
-        #print("start,end step=",bs_start,bs_end,ConvolStep)
+        #print("start,end step=",bs_start,bs_end,Param['ConvolStep'])
         mean = numpy.mean(mkidArr[:,1])
         mkidArr_mean0 = mkidArr - [0,mean]
         
@@ -169,19 +170,19 @@ def lockin(bsArr,interp_BS, mkidFile, force = False, shiftTime = None):
         _mkidDefaultShift = 0.0
         #New correlate method:
         if not shiftTime == None:
-            _mkidShiftErr = mkidShiftErr_useShift
-            _mkidDefaultShift = shiftTime - 0.5 * _mkidShiftErr + mkidBaseShift
+            _mkidShiftErr = Param['mkidShiftErr_useShift']
+            _mkidDefaultShift = shiftTime - 0.5 * _mkidShiftErr + Param['mkidBaseShift']
         else:
-            _mkidShiftErr = mkidShiftErr
-            _mkidDefaultShift = mkidDefaultShift
+            _mkidShiftErr = Param['mkidShiftErr']
+            _mkidDefaultShift = Param['mkidDefaultShift']
         bs_start2 = 0.0
         bs_end2 = 0.0
         shiftstart = 0.0
         shiftend = 0.0
-        if use_costumSE:
-            print("use costum SE: %.3f ~ %.3f" % (mkidShift_costumS, mkidShift_costumE))
-            shiftstart = (1 - mkidShift_costumS) * mkidArr_mean0[0][0] + (mkidShift_costumS) * mkidArr_mean0[-1][0] + _mkidShiftErr
-            shiftend = (1 - mkidShift_costumE) * mkidArr_mean0[0][0] + (mkidShift_costumE) * mkidArr_mean0[-1][0] + _mkidShiftErr
+        if Param['use_costumSE']:
+            print("use costum SE: %.3f ~ %.3f" % (Param['mkidShift_costumS'], Param['mkidShift_costumE']))
+            shiftstart = (1 - Param['mkidShift_costumS']) * mkidArr_mean0[0][0] + (Param['mkidShift_costumS']) * mkidArr_mean0[-1][0] + _mkidShiftErr
+            shiftend = (1 - Param['mkidShift_costumE']) * mkidArr_mean0[0][0] + (Param['mkidShift_costumE']) * mkidArr_mean0[-1][0] + _mkidShiftErr
         else:
             shiftstart = bs_start
             shiftend = bs_end
@@ -189,9 +190,9 @@ def lockin(bsArr,interp_BS, mkidFile, force = False, shiftTime = None):
         bs_end2 = min(mkidArr_mean0[-1][0]+_mkidDefaultShift ,shiftend)
 
         xpoints_mkid = numpy.arange(bs_start2-_mkidShiftErr-_mkidDefaultShift,\
-            bs_end2-_mkidDefaultShift,mkidShiftStep)
+            bs_end2-_mkidDefaultShift,Param['mkidShiftStep'])
     
-        xpoints_BS = numpy.arange(bs_start2,bs_end2,mkidShiftStep)
+        xpoints_BS = numpy.arange(bs_start2,bs_end2,Param['mkidShiftStep'])
         corr1 = interp_mkid_mean0(xpoints_mkid)
         corr2 = -interp_BS(xpoints_BS)+0.5
 
@@ -199,9 +200,9 @@ def lockin(bsArr,interp_BS, mkidFile, force = False, shiftTime = None):
 
         corrArr = scipy.signal.correlate(corr2,corr1,mode = 'valid')
         with open(mkid_file+".corr","wb") as ofs:
-            numpy.savetxt(ofs,numpy.array([[_mkidDefaultShift+x*mkidShiftStep,corrArr[x]] for x in range(0,len(corrArr))]))
+            numpy.savetxt(ofs,numpy.array([[_mkidDefaultShift+x*Param['mkidShiftStep'],corrArr[x]] for x in range(0,len(corrArr))]))
         argmax = numpy.abs(corrArr).argmax()
-        mkidShift = argmax * mkidShiftStep + _mkidDefaultShift
+        mkidShift = argmax * Param['mkidShiftStep'] + _mkidDefaultShift
         if corrArr[argmax] < 0:
             corr_isPositive = False
             print("Warning: detected minus correlation function")
@@ -211,15 +212,15 @@ def lockin(bsArr,interp_BS, mkidFile, force = False, shiftTime = None):
         with open(mkidFile + ".calib","wb") as ofs:
             numpy.savetxt(ofs,mkidArr+[0,mean])
         print("Output calibrated mkid data done!")
-        force = True
+        Param['force'] = True
     print("Mkid shift = ",mkidShift,flush = True)
     #Lock in
-    if not force and os.path.exists(mkidFile+".lockin"):
+    if not Param['force'] and os.path.exists(mkidFile+".lockin"):
         print("Lockin file detected, skip",flush = True)
     else:
         lockin_start2 = math.ceil(max(lockin_start,mkidArr[0][0]))
         lockin_end2 = math.floor(min(lockin_end,mkidArr[-1][0]))
-        max_iter2 = math.floor((lockin_end2 - lockin_start2) / lockin_dt + 0.5)
+        max_iter2 = math.floor((lockin_end2 - lockin_start2) / Param['lockin_dt'] + 0.5)
 
         iter = 1
         sum_ON = 0.0
@@ -231,9 +232,9 @@ def lockin(bsArr,interp_BS, mkidFile, force = False, shiftTime = None):
         print("Begin lockin:",flush=True)
         with progbar.progbar(max_iter2) as pb:
             for point in mkidFiltered:
-                if not point[0] < lockin_start2 + iter * lockin_dt:
+                if not point[0] < lockin_start2 + iter * Param['lockin_dt']:
                     if num_ON > 0 and num_OFF > 0:
-                        lockin_Arr.append([lockin_start2 + (iter - 0.5) * lockin_dt, - sum_ON/num_ON + sum_OFF/num_OFF if corr_isPositive == None or corr_isPositive else sum_ON/num_ON - sum_OFF/num_OFF])
+                        lockin_Arr.append([lockin_start2 + (iter - 0.5) * Param['lockin_dt'], - sum_ON/num_ON + sum_OFF/num_OFF if corr_isPositive == None or corr_isPositive else sum_ON/num_ON - sum_OFF/num_OFF])
                         sum_ON,sum_OFF = 0.0, 0.0
                         num_ON,num_OFF = 0, 0
                         iter = iter + 1
@@ -243,7 +244,7 @@ def lockin(bsArr,interp_BS, mkidFile, force = False, shiftTime = None):
                     else:
                         print("Illigal data has occured!")
                         return
-                if isInMask(point[0], bsArr[:,0], BSFreq, MaskRate):
+                if isInMask(point[0], bsArr[:,0], Param['BSFreq'], Param['MaskRate']):
                     continue
                 if interp_BS(point[0]) > 0.5:
                     #ON
@@ -258,32 +259,54 @@ def lockin(bsArr,interp_BS, mkidFile, force = False, shiftTime = None):
         print("Output lockin data done!",flush=True)
     return (mkidShift,corr_isPositive)
 
-if not len(sys.argv) < 4:
+paramSetRe = re.compile(r"(?P<param>[^=]+)=(?P<value>[^=]+)")
+paramSetList = [x for x in sys.argv if not paramSetRe.match(x) is None]
+intRe = re.compile(r"-?\d+")
+boolRe = re.compile(r"(True)|(False)")
+#set params
+for item in paramSetList:
+    match = paramSetRe.match(item)
+    paramName=match.group('param')
+    if not paramName in Param:
+        continue
+    value=match.group('value')
+    if not intRe.match(value) is None:
+        Param[paramName] = int(value)
+    elif not boolRe.match(value) is None:
+        Param[paramName] = True if value == 'True' else False
+    else:
+        Param[paramName] = float(value)
+    print("Set param %s to %s"%(paramName,value))
+print("now Param:")
+print(Param)
 
-    mkid_file_count = len(sys.argv[3:])
-    if mkid_file_count <= mkidForceCount:
-        force = True
-    bsArr,interp_BS = lockinBS(sys.argv[1],sys.argv[2],force)
+FilteredParam = [x for x in sys.argv if not x in paramSetList]
+if not len(FilteredParam) < 4:
+
+    mkid_file_count = len(FilteredParam[3:])
+    if mkid_file_count <= Param['mkidForceCount']:
+        Param['force'] = True
+    bsArr,interp_BS = lockinBS(FilteredParam[1],FilteredParam[2])
     shiftlist = []
     shiftReadlist = {}
     corrNegativelist = []
     startTime = time.time()
-    if use_shift:
+    if Param['use_shift']:
         if(os.path.exists("shiftlist.txt")):
             shiftReadlist = {math.floor(x[0]+0.5):x[1] for x in numpy.loadtxt("shiftlist.txt")}
             print("Detected shiftlist",flush=True)
         else:
-            use_shift = False
-    for mkid_file in sys.argv[3:]:
+            Param['use_shift'] = False
+    for mkid_file in FilteredParam[3:]:
         mkidNoRe = re.compile("MKID(?P<No>\d\d\d)")
         mkidNoMatch = mkidNoRe.search(mkid_file)
         mkidNo = int(mkidNoMatch.group('No'))
         print("MKID No:",mkidNo,flush=True)
         shift,corr_isPositive = None,None
-        if use_shift:
-            shift,corr_isPositive = lockin(bsArr,interp_BS,mkid_file,force,shiftTime = shiftReadlist[mkidNo])
+        if Param['use_shift']:
+            shift,corr_isPositive = lockin(bsArr,interp_BS,mkid_file,shiftTime = shiftReadlist[mkidNo])
         else:
-            shift,corr_isPositive = lockin(bsArr,interp_BS,mkid_file,force)
+            shift,corr_isPositive = lockin(bsArr,interp_BS,mkid_file)
         #print("Shift = ",shift,flush = True)
         if not shift == None:
             shiftlist.append([float(mkidNo),shift])
@@ -291,15 +314,15 @@ if not len(sys.argv) < 4:
             corrNegativelist.append(mkidNo)
     shiftArr = numpy.array(shiftlist)
     #shiftArr.sort(0)
-    shiftlistName = "shiftlist2.txt" if use_shift else "shiftlist.txt"
-    if not mkid_file_count <= mkidForceCount:
+    shiftlistName = "shiftlist2.txt" if Param['use_shift'] else "shiftlist.txt"
+    if not mkid_file_count <= Param['mkidForceCount']:
         if len(shiftArr) > 0:
             with open(shiftlistName,"wb") as ofs:
                 numpy.savetxt(ofs,shiftArr[shiftArr[:,0].argsort(),:])
             print("Shift list has generated!")
             m = numpy.mean(shiftArr[:,1])
             s = numpy.std(shiftArr[:,1])
-            shiftArr_rejected = [x for x in shiftArr if abs(x[1] - m) >= s*rejectRate]
+            shiftArr_rejected = [x for x in shiftArr if abs(x[1] - m) >= s*Param['rejectRate']]
             if len(shiftArr_rejected) > 0:
                 with open("rejectlist.txt","wb") as ofs:
                     numpy.savetxt(ofs,shiftArr_rejected)
--- lockin/param.py
+++ lockin/param.py
@@ -1,23 +1,25 @@
 #Params
-lockin_dt = 0.25
-mkidDefaultShift = 2.0
-BSFreq = 10.0
-DAQFreq = 80000.0
-FSPFreq = 128.0
-BSSE_TimeSpanRate = 10
-LSE_TimeSpanRate = 0.55
-mkidBaseShift = -0.02
-mkidShiftErr = 4.0
-mkidShiftErr_useShift = 0.05
-mkidShift_costumS = 1.0*(25 - 15)/(80 - 15)
-mkidShift_costumE = 1.0*(35 - 15)/(80 - 15)
-mkidShiftStep = 0.001
-#mkidShiftStep_useShift = 0.0005
-ConvolStep = 0.02
-MaskRate = 0.025
-mkidForceCount = 10
-rejectRate = 2
+Param = {
+'lockin_dt' : 0.25,
+'mkidDefaultShift' : 2.0,
+'BSFreq' : 10.0,
+'DAQFreq' : 80000.0,
+'FSPFreq' : 64.0,
+'BSSE_TimeSpanRate' : 10.0,
+'LSE_TimeSpanRate' : 0.55,
+'mkidBaseShift' : -0.02,
+'mkidShiftErr' : 4.0,
+'mkidShiftErr_useShift' : 0.05,
+'mkidShift_costumS' : 1.0*(55-3.6)/(1221-3.6),
+'mkidShift_costumE' : 1.0*(82 - 3.6)/(1221-3.6),
+'mkidShiftStep' : 0.001,
+#'mkidShiftStep_useShift' : 0.0005,
+#'ConvolStep' : 0.02,
+'MaskRate' : 0.025,
+'mkidForceCount' : 10,
+'rejectRate' : 2,
 
-force = True
-use_shift = True
-use_costumSE = True
+'force' : True,
+'use_shift' : True,
+'use_costumSE' : True
+}
